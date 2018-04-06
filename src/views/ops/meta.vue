d5c2ef2ca5cfa51481a3e31473855342a371e165
余先生 <yolovelife@outlook.com>
2018-04-06 22:29:22 +0800

    完成元操作管理的大部分内容

--- src/views/ops/meta.vue
+++ src/views/ops/meta.vue
@@ -35,8 +35,8 @@
       <el-table-column align="center" label="操作" width="370" class-name="small-padding fixed-width" fixed="right">
         <template slot-scope="meta">
           <el-button type="warning" size="mini" @click="handleRun(meta.row)" :disabled="btnStatus">执行</el-button>
-          <!--<el-button type="warning" size="mini" @click="handleUpdate(meta.row)" :disabled="btnStatus">编辑</el-button>-->
-          <!--<el-button type="danger" size="mini" @click="handleDelete(meta.row)" :disabled="btnStatus">删除</el-button>-->
+          <el-button type="warning" size="mini" @click="handleUpdate(meta.row)" :disabled="btnStatus">编辑</el-button>
+          <el-button type="danger" size="mini" @click="handleDelete(meta.row)" :disabled="btnStatus">删除</el-button>
         </template>
       </el-table-column>
     </el-table>
@@ -59,58 +59,96 @@
     <el-form ref="metaForm" :model="temp" label-position="left" label-width="100px">
       <el-dialog
         width="70%"
-        :title="textMap[dialogStatus]"
-        :visible.sync="dialogCreateVisible">
+        :title="textMap[dialogStatus]+'Step1'"
+        :visible.sync="dialogMetaVisible">
 
             <el-table :data="temp.contents" border fit highlight-current-row
                       style="width: 100%"
                       :default-sort="{prop: 'sort', order: 'ascending'}">
-              <el-table-column width="130px" align="center" label="名称">
+              <el-table-column width="150px" align="center" label="名称">
                 <template slot-scope="content">
                   <span>{{ content.row.name }}</span>
                 </template>
               </el-table-column>
 
-              <el-table-column width="100px" align="center" label="模块">
+              <el-table-column width="150px" align="center" label="模块">
                 <template slot-scope="content">
                   <span>{{ content.row.module }}</span>
                 </template>
               </el-table-column>
 
-              <el-table-column width="100px" align="center" label="参数">
+              <el-table-column width="250px" align="center" label="参数">
                 <template slot-scope="content">
                   <span>{{ content.row.args }}</span>
                 </template>
               </el-table-column>
 
-              <el-table-column width="100px" align="center" label="排序" prop="sort">
+              <el-table-column width="150px" align="center" label="排序" prop="sort">
                 <template slot-scope="content">
                   <span>{{ content.row.sort }}</span>
                 </template>
               </el-table-column>
 
-              <el-table-column width="200px" align="center" label="参数">
+              <el-table-column width="250px" align="center" label="排序工具">
                 <template slot-scope="content">
                   <el-button type="primary" size="mini" @click="handleUp(content.row)" :disabled="btnStatus">上升</el-button>
                   <el-button type="primary" size="mini" @click="handleDown(content.row)" :disabled="btnStatus">下降</el-button>
+                  <el-button type="primary" size="mini" @click="handleContentDelete(content.row)" :disabled="btnStatus">刪除</el-button>
                 </template>
               </el-table-column>
-
             </el-table>
+        <el-input placeholder="拷贝代码" v-model="temp.info">
+          <template slot="prepend">信息: </template>
+        </el-input>
 
         <div slot="footer" class="dialog-footer">
+          <el-button type="primary" @click="handleContentCreate" :disabled="btnStatus">新增操作</el-button>
           <el-button type="primary" @click="handleAsset" :disabled="btnStatus">下一步</el-button>
-          <el-button @click="dialogCreateVisible = false" :disabled="btnStatus">取消</el-button>
+          <el-button @click="dialogMetaVisible = false" :disabled="btnStatus">取消</el-button>
         </div>
+
+        <el-dialog
+          width="30%"
+          title="新增操作"
+          :visible.sync="dialogCreateContentVisible"
+          append-to-body>
+            <div :model="content">
+                <el-input placeholder="检出代码" v-model="content.name">
+                  <template slot="prepend">名称:</template>
+                </el-input>
+                <el-input placeholder="copy" v-model="content.module">
+                  <template slot="prepend">模块:  </template>
+                </el-input>
+                <el-input placeholder="src=/etc/hosts dest=/tmp/hosts" v-model="content.args">
+                  <template slot="prepend">参数:  </template>
+                </el-input>
+            </div>
+            <el-button type="primary" @click="createContent" :disabled="btnStatus">提交</el-button>
+            <el-button @click="dialogCreateContentVisible = false" :disabled="btnStatus">取消</el-button>
+        </el-dialog>
+
       </el-dialog>
 
       <el-dialog
         width="70%"
-        :title="textMap[dialogStatus]"
+        title="确定操作范围Step2"
         :visible.sync="dialogAssetVisible">
+        <el-select v-model="temp.group" placeholder="请选择" @change="fetch_Host">
+          <el-option
+            v-for="item in group_options"
+            :key="item.value"
+            :label="item.label"
+            :value="item.value">
+          </el-option>
+        </el-select>
+
+        <el-transfer v-model="temp.hosts" :data="hosts" placeholder="请选择应用主机IP" filterable>
+        </el-transfer>
+
         <div slot="footer" class="dialog-footer">
           <el-button @click="handleAssetBack" :disabled="btnStatus">上一步</el-button>
-          <el-button @click="createMeta" :disabled="btnStatus">提交</el-button>
+          <el-button v-if="dialogStatus=='create'" type="primary" @click="createMeta" :disabled="btnStatus">提交</el-button>
+          <el-button v-else type="primary" @click="updateMeta" :disabled="btnStatus">提交</el-button>
         </div>
       </el-dialog>
     </el-form>
@@ -119,9 +157,9 @@
 </template>
 
 <script>
-  import { fetch_MetaListByPage } from '@/api/ops';
+  import { fetch_MetaListByPage,create_Meta,update_Meta,delete_Meta } from '@/api/ops';
+  import { fetch_GroupList,fetch_HostList } from '@/api/manager';
   import Xterm from '@/components/Xterm/index';
-  import ElRow from "element-ui/packages/row/src/row";
   export default {
     data(){
       return{
@@ -131,38 +169,29 @@
         dialogStatus:'',
         textMap:{
           create: '新建元操作',
-          asset: '确定操作范围',
+          update: '修改元操作'
         },
         pagination: {
           page: 1,
           count: 0
         },
+        group_options:[],
+        hosts:[],
         dialogXtermVisible:false,
-        dialogCreateVisible:false,
+        dialogMetaVisible:false,
         dialogAssetVisible:false,
+        dialogCreateContentVisible:false,
         meta_id: null,
+        content: {
+
+        },
         temp: {
-          contents: [
-            {
-              "id": 1,
-              "name": "li",
-              "module": "shell",
-              "args": "ls -lh",
-              "sort": 2
-            },
-            {
-              "id": 2,
-              "name": "zi",
-              "module": "shell",
-              "args": "touch /ddddr",
-              "sort": 1
-            }
-          ]
+          contents: [],
+          hosts: []
         }
       }
     },
     components: {
-      ElRow,
       Xterm
     },
     created(){
@@ -196,65 +225,181 @@
           this.dialogXtermVisible = false
         })
       },
+      resetContent(){
+        this.content = {
+        }
+      },
       resetTemp(){
         this.temp={
-          contents: [
-            {
-              "id": 1,
-              "name": "li",
-              "module": "shell",
-              "args": "ls -lh",
-              "sort": 2
-            },
-            {
-              "id": 2,
-              "name": "zi",
-              "module": "shell",
-              "args": "touch /ddddr",
-              "sort": 1
-            }
-          ]
+          contents: [],
+          hosts:[]
         }
+      },
+      fetch_Host(value){
+        fetch_HostList(value).then(response=>{
+          this.hosts = []
+          for (const host of response.data){
+            this.hosts.push({
+              key: host.id,
+              label: host.hostname,
+              disabled: false
+            })
+          }
+        })
+      },
+      fetch_Group(){
+        fetch_GroupList().then(response=>{
+          this.group_options = []
+          for (const group of response.data){
+            this.group_options.push({
+              value: group.id,
+              label: group.name,
+              disabled: false
+            })
+          }
+        })
       },
       handleCreate(){
         this.resetTemp()
-        console.log(this.temp)
         this.dialogStatus = 'create'
-        this.dialogCreateVisible = true
+        this.dialogMetaVisible = true
         this.$nextTick(() => {
           this.$refs['metaForm'].clearValidate()
         })
       },
+      handleUpdate(row){
+        this.temp = Object.assign({}, row) // copy obj
+        this.dialogStatus = 'update'
+        this.dialogMetaVisible = true
+        this.fetch_Host(this.temp.group)
+        this.$nextTick(() => {
+          this.$refs['metaForm'].clearValidate()
+        })
+      },
+      handleDelete(row){
+        this.temp = Object.assign({},row)
+        this.btnStatus=true
+        this.deleteConfirm()
+        this.btnStatus=false
+      },
+      deleteConfirm() {
+        this.$confirm('此操作将删除元操作, 是否继续?', '提示', {
+          confirmButtonText: '确定',
+          cancelButtonText: '取消',
+          type: 'warning'
+        }).then(()=>{
+          delete_Meta(this.temp).then((response) => {
+            this.$message({
+              showClose: true,
+              message: '删除成功',
+              type: 'success'
+            })
+            this.init()
+          })
+        })
+      },
+      handleContentCreate()
+      {
+        this.dialogCreateContentVisible = true
+        this.resetContent()
+      },
       handleAsset(){
-        this.dialogStatus = 'asset'
-        this.dialogCreateVisible = false
+        this.fetch_Group()
+        this.dialogMetaVisible = false
         this.dialogAssetVisible = true
       },
       handleAssetBack(){
         this.dialogStatus = 'create'
         this.dialogAssetVisible = false
-        this.dialogCreateVisible = true
+        this.dialogMetaVisible = true
+      },
+      sortContents(){
+        this.temp.contents.sort(function(a,b){
+          return a.sort - b.sort
+        })
       },
       handleUp(row){
-        if(this.temp.contents.length>1){
-          for(var i=1; i<this.temp.contents.length; i++) {
-            if(row.sort == this.temp.contents[i].sort){ // 如果匹配的話
-              console.log(this.temp.contents[i-1].name,this.temp.contents[i-1].sort)
-              console.log(this.temp.contents[i-1].name,this.temp.contents[i].sort)
-              this.temp.contents[i].sort = this.temp.contents[i].sort -1
-              this.temp.contents[i-1].sort = this.temp.contents[i-1].sort +1
-              console.log(this.temp.contents[i-1].name,this.temp.contents[i-1].sort)
-              console.log(this.temp.contents[i].name,this.temp.contents[i].sort)
-
+        if(row.sort==1){// 如果要處理的條目已經是最上面一層
+          return
+        }
+        if(this.temp.contents.length>1){ // 如果要處理的條目確實需要上升=
+          for(var i=0; i<this.temp.contents.length; i++) {
+            if(this.temp.contents[i].sort == row.sort -1){
+              row.sort--
+              this.temp.contents[i].sort++
             }
           }
         }
+        this.sortContents()
       },
       handleDown(row){
-
+        if(row.sort == this.temp.contents.length){ // 如果要處理的條目已經是最下面一層
+          return
+        }
+        if(this.temp.contents.length>1){
+          for(var i=0;i<this.temp.contents.length;i++){
+            if(this.temp.contents[i].sort == row.sort +1){
+              row.sort++
+              this.temp.contents[i].sort--
+            }
+          }
+        }
+        this.sortContents()
+      },
+      handleContentDelete(row){
+        for(var i=0;i<this.temp.contents.length;i++){
+          if(this.temp.contents[i].sort == row.sort){
+            this.temp.contents.splice(i,1)
+          }
+        }
+      },
+      createContent(){
+        this.content.sort = this.temp.contents.length+1
+        this.temp.contents.push(this.content)
+        this.sortContents()
+        this.dialogCreateContentVisible=false
       },
       createMeta(){
-
+        this.$refs['metaForm'].validate((valid) => {
+          if (valid) {
+            this.btnStatus=true
+            create_Meta(this.temp).then(() => {
+              this.init()
+              this.dialogAssetVisible = false
+              this.$message({
+                showClose: true,
+                message: '创建元操作成功',
+                type: 'success'
+              })
+              this.btnStatus=false
+            }).catch((error)=>{
+              this.btnStatus=false
+              this.dialogAssetVisible = false
+              console.log(error)
+            })
+          }
+        })
+      },
+      updateMeta(){
+        this.$refs['metaForm'].validate((valid) => {
+          if (valid) {
+            this.btnStatus=true
+            update_Meta(this.temp).then(() => {
+              this.init()
+              this.dialogAssetVisible = false
+              this.$message({
+                showClose: true,
+                message: '更新元操作成功',
+                type: 'success'
+              })
+              this.btnStatus=false
+            }).catch((error)=>{
+              this.btnStatus=false
+              this.dialogAssetVisible = false
+              console.log(error)
+            })
+          }
+        })
       }
     }
   }
